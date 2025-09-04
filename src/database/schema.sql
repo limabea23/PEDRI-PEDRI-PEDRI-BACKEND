bd6b5a56f9a17ebb36a96a37b30ab838f28d61f8
Beatriz L <158210831+limabea23@users.noreply.github.com>
2025-09-04 16:06:56 -0300

    feat: create database and tables

---
+++ src/database/schema.sql
@@ -0,0 +1,37 @@
+CREATE DATABASE pedri;
+
+\c pedri;
+
+CREATE TABLE listagem (
+    id SERIAL PRIMARY KEY,
+    conteudo VARCHAR(100) NOT NULL UNIQUE
+);
+
+CREATE TABLE detalhes (
+    id SERIAL PRIMARY KEY,
+    listagem_id INT NOT NULL,
+    titulo VARCHAR(100) NOT NULL,
+    subtitulo TEXT,
+    texto TEXT, 
+    anexo TEXT, 
+    CONSTRAINT fk_listagem_detalhes FOREIGN KEY (listagem_id) REFERENCES listagem(id) 
+);
+
+CREATE TABLE galeria (
+    id SERIAL PRIMARY KEY,
+    listagem_id INT NOT NULL,
+    titulo VARCHAR(100) NOT NULL,
+    anexo TEXT, 
+    CONSTRAINT fk_listagem_galeria FOREIGN KEY (listagem_id) REFERENCES listagem(id) 
+);
+
+CREATE TABLE noticias (
+    id SERIAL PRIMARY KEY,
+    listagem_id INT NOT NULL,
+    titulo VARCHAR(100) NOT NULL,
+    subtitulo TEXT,
+    texto TEXT, 
+    anexo TEXT, 
+    data_publicacao DATE,
+    CONSTRAINT fk_listagem_noticias FOREIGN KEY (listagem_id) REFERENCES listagem(id) 
+);
