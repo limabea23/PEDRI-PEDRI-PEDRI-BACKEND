1aec797c06324350e4d9c76d24f9fe3640a96adb
Beatriz L <beatrizlima.tds1.senai@gmail.com>
2025-09-08 22:26:51 -0300

    update: server.js

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const listagemRoutes = require("./src/routes/listagemRoutes");
+const detalhesRoutes = require("./src/routes/detalhesRoutes");
+const galeriaRoutes = require("./src/routes/galeriaRoutes");
+const noticiasRoutes = require("./src/routes/noticiasRoutes");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use("/api", listagemRoutes);
+app.use("/api", detalhesRoutes);
+app.use("/api", galeriaRoutes);
+app.use("/api", noticiasRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
